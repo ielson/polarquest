20e99189886682459fcd82c167d1930985f6e41d
Daniel Mascarenhas <daniel.mascarenhas@saipem.com>
2026-02-26 16:05:37 -0300

    feat: fixed driver node to work with car_like and recorded bag

--- ws/src/polaris_system_id/src/driver.py
+++ ws/src/polaris_system_id/src/driver.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 import rospy, random
-from ackermann_msgs.msg import AckermannDrive
+from ackermann_msgs.msg import AckermannDriveStamped
 
 def clamp(x, lo, hi):
     return max(lo, min(hi, x))
 
 def main():
     rospy.init_node("sys_id_driver")
-    pub = rospy.Publisher("/gem/ackermann_cmd", AckermannDrive, queue_size=1)
+    pub = rospy.Publisher("/car_like/ackermann_cmd", AckermannDriveStamped, queue_size=1)
 
     # === parâmetros ===
     rate_hz     = rospy.get_param("~rate_hz", 30)          
@@ -31,7 +31,8 @@ def main():
 
     random.seed(seed)
 
-    msg = AckermannDrive()
+    msg = AckermannDriveStamped()
+    msg.header.stamp = rospy.Time.now()
     rate = rospy.Rate(rate_hz)
     Ts = 1.0 / rate_hz
 
@@ -49,29 +50,29 @@ def main():
             if seg != last_seg:
                 if seg == 0:
                     # always starts sending a speed with 0 heading
-                    msg.speed = clamp(fixed_speed, min_speed, speed_max)
-                    msg.steering_angle = 0.0
+                    msg.drive.speed = clamp(fixed_speed, min_speed, speed_max)
+                    msg.drive.steering_angle = 0.0
                     rospy.loginfo("[seg=%d] PRE  | speed=%.2f | delta=%.3f",
-                                  seg, msg.speed, msg.steering_angle)
+                                  seg, msg.drive.speed, msg.drive.steering_angle)
                 elif seg == 1:
                     # direction jstep, maintaining fixed speed
-                    msg.speed = clamp(fixed_speed, min_speed, speed_max)
-                    msg.steering_angle = clamp(delta_step, -steer_max, steer_max)
+                    msg.drive.speed = clamp(fixed_speed, min_speed, speed_max)
+                    msg.drive.steering_angle = clamp(delta_step, -steer_max, steer_max)
                     rospy.loginfo("[seg=%d] STEP | speed=%.2f | delta=%.3f",
-                                  seg, msg.speed, msg.steering_angle)
+                                  seg, msg.drive.speed, msg.drive.steering_angle)
                 else:
                     new_speed = random.uniform(min_speed, speed_max)
                     new_steer = random.uniform(-steer_max, steer_max)
-                    msg.speed = clamp(new_speed, min_speed, speed_max)
-                    msg.steering_angle = clamp(new_steer, -steer_max, steer_max)
+                    msg.drive.speed = clamp(new_speed, min_speed, speed_max)
+                    msg.drive.steering_angle = clamp(new_steer, -steer_max, steer_max)
                     rospy.loginfo("[seg=%d] VAR  | speed=%.2f | delta=%.3f",
-                                  seg, msg.speed, msg.steering_angle)
+                                  seg, msg.drive.speed, msg.drive.steering_angle)
                 last_seg = seg
 
             pub.publish(msg)
 
             if int(t * rate_hz) % rate_hz == 0:
-                rospy.loginfo("t=%.1fs | speed=%.2f | delta=%.3f", t, msg.speed, msg.steering_angle)
+                rospy.loginfo("t=%.1fs | speed=%.2f | delta=%.3f", t, msg.drive.speed, msg.drive.steering_angle)
 
             t += Ts
             rate.sleep()
@@ -83,21 +84,21 @@ def main():
         t = 0.0
         while not rospy.is_shutdown() and t < total_time:
             if t < pre_time:
-                msg.speed = 0.0
-                msg.steering_angle = 0.0
+                msg.drive.speed = 0.0
+                msg.drive.steering_angle = 0.0
             else:
-                msg.speed = speed_step
-                msg.steering_angle = 0.0
+                msg.drive.speed = speed_step
+                msg.drive.steering_angle = 0.0
 
             pub.publish(msg)
             if int(t * rate_hz) % rate_hz == 0:
-                rospy.loginfo("t=%.1fs | speed=%.2f | delta=%.3f", t, msg.speed, msg.steering_angle)
+                rospy.loginfo("t=%.1fs | speed=%.2f | delta=%.3f", t, msg.drive.speed, msg.drive.steering_angle)
 
             t += Ts
             rate.sleep()
 
-    msg.speed = 0.0
-    msg.steering_angle = 0.0
+    msg.drive.speed = 0.0
+    msg.drive.steering_angle = 0.0
     pub.publish(msg)
     rospy.loginfo("Finished sending messages for sysId")
 
